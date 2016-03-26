5e0bc6af831ae3ec67727a631bba0ce09709f81e
See.Ku <info@see-ku.com>
2016-03-26 13:12:36 +0900

    とりあえずコミット

---
+++ SK4ToolDemo/Notify1ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  Notify1ViewController.swift
+//  SK4ToolDemo
+//
+//  Created by See.Ku on 2016/03/26.
+//  Copyright (c) 2016 AxeRoad. All rights reserved.
+//
+
+import UIKit
+
+class Notify1ViewController: UIViewController {
+
+	@IBOutlet weak var statusLabel: UILabel!
+	@IBOutlet weak var textField: UITextField!
+
+	@IBAction func onBlue(sender: AnyObject) {
+	}
+
+	@IBAction func onBlack(sender: AnyObject) {
+	}
+
+	@IBAction func onRed(sender: AnyObject) {
+	}
+
+	@IBAction func onGreen(sender: AnyObject) {
+	}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
