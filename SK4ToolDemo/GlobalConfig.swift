083b8d56a3c401474d9f1d458da64a0958de49af
See.Ku <info@see-ku.com>
2016-03-31 18:30:10 +0900

    とりあえずコミット

--- SK4ToolDemo/GlobalConfig.swift
+++ SK4ToolDemo/GlobalConfig.swift
@@ -13,7 +13,19 @@ class GlobalConfig: SK4ConfigUserDefaults {
 	override func onSetup() {
 		title = "Test Config"
 
+
+		testBool()
+		testIndex()
+		testIndexSegmented()
+
+
 		testInt()
+		testCGFloat()
+		testUInt64()
+		testString()
+
+
+
 
 
 		commonInfo()
@@ -32,6 +44,131 @@ class GlobalConfig: SK4ConfigUserDefaults {
 		int2.cell = SK4ConfigCellTextField(maxLength: 8)
 	}
 
+	// /////////////////////////////////////////////////////////////
+	// for SK4ConfigCGFloat
+
+	let cgfloat1 = SK4ConfigCGFloat(title: "Label", value: 100.0)
+	let cgfloat2 = SK4ConfigCGFloat(title: "TextField", value: 100.0)
+
+	func testCGFloat() {
+		let sec = addUserSection("Test CGFloat")
+		sec.addConfig(cgfloat1)
+		sec.addConfig(cgfloat2)
+		cgfloat2.cell = SK4ConfigCellTextField(maxLength: 8)
+	}
+
+	// /////////////////////////////////////////////////////////////
+	// for SK4ConfigUInt64
+
+	let ui64_1 = SK4ConfigUInt64(title: "Label", value: 12345678)
+	let ui64_2 = SK4ConfigUInt64(title: "TextField", value: 87654321)
+
+	func testUInt64() {
+		let sec = addUserSection("Test UInt64")
+		sec.addConfig(ui64_1)
+		sec.addConfig(ui64_2)
+		ui64_2.cell = SK4ConfigCellTextField(maxLength: 24)
+	}
+
+	// /////////////////////////////////////////////////////////////
+	// for SK4ConfigString
+
+	let string1 = SK4ConfigString(title: "Label", value: "Info")
+	let string3 = SK4ConfigString(title: "TextView (No limit)", value: "Input", maxLength: 0)
+	let string4 = SK4ConfigString(title: "TextField (Max 10)", value: "Max 10", maxLength: 10)
+	let string5 = SK4ConfigString(title: "ReadOnly", value: "Max 10", maxLength: 10)
+
+	func testString() {
+		let sec = addUserSection("Test String")
+		sec.addConfig(string1)
+		sec.addConfig(string3)
+		sec.addConfig(string4)
+		sec.addConfig(string5)
+		string5.readOnly = true
+	}
+
+	// /////////////////////////////////////////////////////////////
+	// for SK4ConfigBool
+
+	let bool1 = SK4ConfigBool(title: "ReadOnly (true)", value: true)
+	let bool2 = SK4ConfigBool(title: "ReadOnly (false)", value: false)
+	let bool4 = SK4ConfigBool(title: "TextField", value: false)
+	let bool5 = SK4ConfigBool(title: "Switch", value: true)
+
+	func testBool() {
+		let sec = addUserSection("Test Bool")
+		sec.addConfig(bool1)
+		sec.addConfig(bool2)
+		sec.addConfig(bool4)
+		sec.addConfig(bool5)
+
+		bool1.readOnly = true
+		bool2.readOnly = true
+		bool4.cell = SK4ConfigCellTextField(maxLength: 4)
+	}
+
+	// /////////////////////////////////////////////////////////////
+	// for SK4ConfigIndex
+
+	let stringDetail1 = SK4ConfigIndex(title: "Index", value: 0)
+	let stringDetail2 = SK4ConfigIndex(title: "Index + Detail", value: 0)
+	let stringDetail3 = SK4ConfigIndex(title: "Index / Read Only", value: 0)
+	let stringDetail4 = SK4ConfigIndex(title: "Index + Detail / Read Only", value: 0)
+
+	func testIndex() {
+		let sec = addUserSection("Test Index")
+		sec.addConfig(stringDetail1)
+		sec.addConfig(stringDetail2)
+		sec.addConfig(stringDetail3)
+		sec.addConfig(stringDetail4)
+
+		let select = [
+			"Black1", "Blue1", "Red1", "Green1", "Yellow1", "White1",
+			"Black2", "Blue2", "Red2", "Green2", "Yellow2", "White3",
+			"Black3", "Blue3", "Red3", "Green3", "Yellow3", "White4",
+			]
+
+		let detail = [
+			"黒1", "青1", "赤1", "緑1", "黄1", "白1",
+			"黒2", "青2", "赤2", "緑2", "黄2", "白2",
+			"黒3", "青3", "赤3", "緑3", "黄3", "白3",
+			]
+
+		stringDetail1.choices = select
+
+		stringDetail2.choices = select
+		stringDetail2.details = detail
+
+		stringDetail3.choices = select
+		stringDetail3.readOnly = true
+
+		stringDetail4.choices = select
+		stringDetail4.details = detail
+		stringDetail4.readOnly = true
+	}
+
+	// /////////////////////////////////////////////////////////////
+	// for SK4ConfigIndex + Segmented
+
+	let stringSegmented1 = SK4ConfigIndex(title: "Seg 2", value: 0)
+	let stringSegmented2 = SK4ConfigIndex(title: "Seg 3", value: 0)
+	let stringSegmented3 = SK4ConfigIndex(title: "ReadOnly", value: 0)
+
+	func testIndexSegmented() {
+		let sec = addUserSection("Test Index + Segmented")
+		sec.addConfig(stringSegmented1)
+		sec.addConfig(stringSegmented2)
+		sec.addConfig(stringSegmented3)
+
+		stringSegmented1.cell = SK4ConfigCellSegmented()
+		stringSegmented2.cell = SK4ConfigCellSegmented()
+		stringSegmented3.cell = SK4ConfigCellSegmented()
+
+		stringSegmented1.choices = ["White", "Black"]
+		stringSegmented2.choices = ["Red", "Blue", "Green"]
+		stringSegmented3.choices = ["Red", "Blue", "Green"]
+		stringSegmented3.readOnly = true
+	}
 
 
 
